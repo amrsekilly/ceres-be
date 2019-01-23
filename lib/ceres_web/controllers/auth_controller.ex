02eac8ef76880e6cf990221f3943163a2784fa42
Amr Elsekilly <github@amrsekilly.com>
2019-01-23 05:01:54 +0200

    Sent back the user data after a successful login

--- lib/ceres_web/controllers/auth_controller.ex
+++ lib/ceres_web/controllers/auth_controller.ex
@@ -7,35 +7,16 @@ defmodule CeresWeb.AuthController do
 
   @spec login(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def login(conn, %{"code" => code}) do
-    code
-    |> Slack.getSlackOauth()
-    |> Poison.decode!()
-    |> signin(conn)
+    user =
+      code
+      |> Slack.getSlackOauth()
+      |> Poison.decode!()
+      |> signin(conn)
 
-    render(conn, "user.json", code: code)
+    # IO.inspect(user)
+
+    render(conn, "user.json", user: user)
   end
-
-  # def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-  #   %{"image_192" => image_192} = auth.info.urls
-
-  #   user_params = %{
-  #     slack_token: auth.credentials.token,
-  #     slack_id: auth.info.nickname,
-  #     name: auth.info.name,
-  #     avatar: image_192
-  #   }
-  #   IO.inspect "---------------------------------"
-  #   IO.inspect "---------------------------------"
-  #   IO.inspect "---------------------------------"
-  #   User.changeset(%User{}, user_params)
-  #   |> IO.inspect
-  #   |> signin(conn)
-  # end
-
-  # def signout(conn, _) do
-  #   conn
-  #   |> IO.inspect()
-  # end
 
   defp insert_update_user(%{
          "user" => %{"id" => slack_id, "name" => name, "image_192" => image_192},
@@ -67,8 +48,7 @@ defmodule CeresWeb.AuthController do
         |> IO.inspect()
 
       {:ok, user} ->
-        conn
-        |> IO.inspect()
+        user
     end
   end
 end
--- lib/ceres_web/views/auth_view.ex
+++ lib/ceres_web/views/auth_view.ex
@@ -2,9 +2,11 @@ defmodule CeresWeb.AuthView do
   use CeresWeb, :view
   import CeresWeb.JsonHelpers
 
-  def render("user.json", %{code: code}) do
-    # %{name: user.name, avatar: user.avatar, wallet: user.wallet, isAdmin: user.is_admin}
+  def render("user.json", %{
+        user: %Ceres.Accounts.User{name: name, avatar: avatar, wallet: wallet, is_admin: isAdmin}
+      }) do
+    # %{}
     # |> format_json_response
-    %{code: code}
+    %{name: name, avatar: avatar, wallet: wallet, isAdmin: isAdmin}
   end
 end
