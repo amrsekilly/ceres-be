9c8b281b9013805cd3e619358f8bed20720b79f2
Amr Elsekilly <github@amrsekilly.com>
2019-01-27 03:40:33 +0200

    Changed the structure for the wallet, to allow having a personal deposit. Resolves 34

--- priv/repo/migrations/20190127013238_change_wallet_structure.exs
+++ priv/repo/migrations/20190127013238_change_wallet_structure.exs
@@ -3,7 +3,6 @@ defmodule Ceres.Repo.Migrations.ChangeWalletStructure do
 
   def change do
     alter table(:users) do
-      # modify(:wallet, :float, default: 0.0)
       remove(:wallet)
       add(:allowance, :float, default: 0.0)
       add(:personal_deposit, :float, default: 0.0)
